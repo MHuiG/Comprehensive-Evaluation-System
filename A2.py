1c7f02718f16e8337437e720bf3b1352548d082e
BLueberry_Pie <38281581+BLueberryPieBLue@users.noreply.github.com>
2018-12-20 22:00:49 +0800

    Update and rename 1.2智育成绩汇总.py to A2.py

---
+++ A2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#
+import os
+import xlrd
+import xlwt
+
+
+def A2():
+    path = "./Score"
+    files = os.listdir(path)
+    workbook = xlwt.Workbook()
+    sheet1 = workbook.add_sheet('sheet1',cell_overwrite_ok=True)
+    flag=0
+    x=0
+    y=0
+    for file in files:
+        f = os.path.basename(file)
+        data = xlrd.open_workbook(os.path.join(path,f))
+        table = data.sheets()[0]
+        if flag==0:
+            for i in table.row_values(0):
+                sheet1.write(x,y,i)
+                y+=1
+            flag=1
+            x+=1
+            y=0
+        for i in table.row_values(1):
+            sheet1.write(x,y,i)
+            y+=1
+        x+=1
+        y=0
+    workbook.save('./智育成绩汇总.xls')
+
+
+if __name__=="__main__":
+    A2()
+
