bc08ee19f47334dd5a095feb6d70a75ca2a887e2
BLueberry_Pie <38281581+BLueberryPieBLue@users.noreply.github.com>
2018-12-20 22:19:57 +0800

    Update A4.py

--- A4.py
+++ A4.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 #
+#A4.py 智育成绩数据库操作
+#1.计算智育成绩
+#    智育成绩=(学科成绩*学科学分)之和/(学科学分)之和
+#2.排序
+#3.导出"智育成绩.xls"
+#    格式:序号 学号 姓名 (学科) 智育总分
+#
 import sqlite3
 import os
 import xlrd
