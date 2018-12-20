4cc10e26a6baeb0e592ea8283c393baa7c42b519
BLueberry_Pie <38281581+BLueberryPieBLue@users.noreply.github.com>
2018-12-20 22:23:27 +0800

    Update B3.py

--- B3.py
+++ B3.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 #
+#B3.py 综测成绩导入数据库
+#1.链接数据库"Score.db"
+#2.创建数据表"ZScore"，设置主键"学号 char(20) PRIMARY KEY"
+#    数据表格式:学号 姓名 思想品德得分（10%） 身心素质得分（5%）创新实践能力得分（10%） 学院特色得分（5%）
+#3.将"综测成绩汇总.xls"导入数据库"ZScore.db"
+#
 import sqlite3
 import os
 import xlrd
