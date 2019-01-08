6db1709058bac9772c413ecb7ead08609163f37e
Feign-harder-coder <46022198+Feign-harder-coder@users.noreply.github.com>
2019-01-08 22:10:43 +0800

    Update B3.py

--- B3.py
+++ B3.py
@@ -15,4 +15,64 @@ import xlwt
 def B3():
     pass
 if __name__=="__main__":
-    pass
+    cn = sqlite3.connect('ZScore.db')
+    cur = cn.cursor()
+
+    path = "./综测成绩汇总.xls"
+    data = xlrd.open_workbook(path)
+    table = data.sheets()[0]
+    ncols = table.ncols
+    nrows = table.nrows
+    s1 = "学号 char(20) PRIMARY KEY, 姓名 char(10), "
+    flag = 0
+    for i in table.row_values(0):
+        if flag >= 2:
+            if flag%2==0:
+                s1 = s1 + i + ' char(1000)'
+            if flag%2!=0:
+                s1 = s1 + i +' DOUBLE'
+            if flag != ncols - 1:
+                s1 = s1 + ','
+        flag += 1
+
+    try:
+        print('''CREATE TABLE IF NOT EXISTS ZScore(''' + s1 + ''');''')
+        cn.execute('''CREATE TABLE IF NOT EXISTS ZScore(''' + s1 + ''');''')
+    except:
+        pass
+    s2 = ''
+    flag = 0
+    for i in table.row_values(0):
+        s2 = s2 + i
+        if flag != ncols - 1:
+            s2 = s2 + ','
+        flag += 1
+
+    for i in range(nrows):
+        flag = 0
+        s3 = ''
+        if i != 0:
+            for k in table.row_values(i):
+                if flag == 0:
+                    s3 = s3 + str(int(k))
+                elif flag == 1:
+                    s3 = s3 + "'" + str(k) + "'"
+                elif flag ==2:
+                    s3 = s3 + "'" + str(k) + "'"
+                elif flag == 4:
+                    s3 = s3 + "'" + str(k) + "'"
+                elif flag == 6:
+                    s3 = s3 + "'" + str(k) + "'"
+                elif flag == 8:
+                    s3 = s3 + "'" + str(k) + "'"
+                else:
+                    s3 = s3 + str(k)
+                if flag != ncols - 1:
+                    s3 += ','
+                    flag += 1
+                    # print(flag)
+            print('''insert into  ZScore (''' + s2 + ''') values(''' + s3 + ''')''')
+            cn.execute('''insert into  ZScore (''' + s2 + ''') values(''' + s3 + ''')''')
+            cn.commit()
+    cur.close()
+    cn.close()
