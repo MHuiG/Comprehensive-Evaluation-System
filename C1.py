6d6f1a78c4f77e26706874167902cb4c6ffb7472
BLueberry_Pie <38281581+BLueberryPieBLue@users.noreply.github.com>
2019-01-09 20:45:07 +0800

    Update C1.py

--- C1.py
+++ C1.py
@@ -11,5 +11,70 @@ import xlwt
 
 def C1():
     pass
+def select_all():
+    cn = sqlite3.connect('Score.db')
+    cur = cn.cursor()
+    s="""select * from Score"""
+    cursor=cn.execute(s)
+    col_name_list = [tuple[0] for tuple in cursor.description]
+    print (col_name_list)
+    for row in cursor:
+        print(row)
+    cur.close()
+    cn.close()
+def select_by_xh(xh):
+    cn = sqlite3.connect('Score.db')
+    cur = cn.cursor()
+    s="""select * from Score where 学号= '"""+str(xh)+"""'"""
+    cursor=cn.execute(s)
+    col_name_list = [tuple[0] for tuple in cursor.description]
+    print (col_name_list)
+    for row in cursor:
+        print(row)
+    cur.close()
+    cn.close()
+
+def insert(L):
+    s=""
+    z=0
+    for i in L:
+        if z==0:
+            s+="'"+str(i)+"'"
+        else:
+            s+=",'"+str(i)+"'"
+        z=z+1
+    cn = sqlite3.connect('Score.db')
+    cur = cn.cursor()
+    s="""insert into Score values("""+s+""")"""
+    cursor=cn.execute(s)
+    cn.commit()
+    select_all()
+def delete_by_xh(xh):
+    cn = sqlite3.connect('Score.db')
+    cur = cn.cursor()
+    s="""delete from Score where 学号= '"""+str(xh)+"""'"""
+    cursor=cn.execute(s)
+    cn.commit()
+    select_all()
+def update(xh,a,b):
+    cn = sqlite3.connect('Score.db')
+    cur = cn.cursor()
+    s="""update Score set """+str(a)+""" = '"""+str(b)+"""' where 学号= '"""+str(xh)+"""'"""
+    cursor=cn.execute(s)
+    cn.commit()
+    select_all()
+
 if __name__=="__main__":
     pass
+    #select_all()
+        
+    #L=['171214080267344','0', 73.0, 82.8, 87.7, 79.7, 65.8, 63.6, 76.0, 83.7, 86.0, 92.0, 81.5, 86.0]
+    #insert(L)
+    
+    
+    #update('171214080267344','111','0')
+
+    #select_by_xh('171214080267344')
+    
+    
+    #delete_by_xh(171214080267344)
