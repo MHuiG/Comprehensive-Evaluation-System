4ce80d8b5e2c3d04182ac493dd3cbe40e7cbe89b
nijiagebilaowang <46022844+nijiagebilaowang@users.noreply.github.com>
2019-01-10 21:21:16 +0800

    Update A4.py

--- A4.py
+++ A4.py
@@ -15,5 +15,61 @@ import xlwt
 
 def A4():
     pass
+def order():
+    workbook = xlwt.Workbook()
+    sheet1 = workbook.add_sheet('sheet1',cell_overwrite_ok=True)
+
+    style = xlwt.easyxf('align: wrap on,horz center,vert center')
+
+    cn = sqlite3.connect('Score.db')
+    cur = cn.cursor()
+    s="""select * from Score order by "智育成绩" desc"""
+    cursor=cn.execute(s)
+    col_name_list = [tuple[0] for tuple in cursor.description]
+    #print (col_name_list)
+    for i in range(len(col_name_list)):
+        sheet1.write(0,i,col_name_list[i],style)
+        #print(col_name_list[i])
+    a=1
+    for row in cursor:
+        #print(row)
+        for i in range(len(row)):
+            sheet1.write(a,i,row[i],style)
+            #print(row[i])
+        a=a+1   
+    workbook.save('./智育成绩.xls')
+    cur.close()
+    cn.close()
 if __name__=="__main__":
-    pass
+    cn = sqlite3.connect('Score.db')
+    cur = cn.cursor()
+    s="""select * from Credits limit 1"""
+    cursor=cn.execute(s)
+    for row in cursor:
+        xuefen=row
+
+    print(xuefen)
+    try:
+        cn.execute('''alter table Score add COLUMN '智育成绩' DOUBLE''')
+    except:
+        print('Table has Exist')
+    cur = cn.execute('select * from Score')
+    while True:
+        x=cur.fetchone()
+
+        if not x:break
+        n=len(x)
+        k=0
+        sum=0
+        for i in range(n-3):
+            k=k+x[i+2]*xuefen[i]
+            sum=sum+xuefen[i]
+        k=k/sum
+        print(k)
+        s="""update Score set '智育成绩'="""+str(k)+""" where 学号 = '"""+str(x[0])+"""'"""
+        cn.execute(s)
+        cn.commit()
+    order()
+
+    cur.close()
+    cn.close()
