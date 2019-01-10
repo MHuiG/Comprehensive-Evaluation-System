1ceaf6340902756832659ebb941f253e121cfbf0
Feign-harder-coder <46022198+Feign-harder-coder@users.noreply.github.com>
2019-01-10 20:28:11 +0800

    Update B4.py

--- B4.py
+++ B4.py
@@ -18,7 +18,7 @@ def B4():
 def order():
     cn = sqlite3.connect('Score.db')
     cur = cn.cursor()
-    s="""select * from Score order by "综测成绩" desc"""
+    s="""select * from ZScore order by "综测成绩" desc"""
     cursor=cn.execute(s)
     col_name_list = [tuple[0] for tuple in cursor.description]
     print (col_name_list)
@@ -29,10 +29,10 @@ def order():
     cn.close()
 if __name__=="__main__":
     cn = sqlite3.connect('Score.db')
-    cur = cn.execute('select * from Score')
+    cur = cn.execute('select * from ZScore')
     i = 0
     try:
-        cn.execute('''alter table Score add COLUMN '综测成绩' DOUBLE''')
+        cn.execute('''alter table ZScore add COLUMN '综测成绩' DOUBLE''')
     except:
         print('Table has Exist')
 
@@ -40,7 +40,7 @@ if __name__=="__main__":
         x=cur.fetchone()
         if not x:break
         k=(x[10]*0.7)+(x[3]*0.1)+(x[5]*0.05)+(x[7]*0.1)+(x[9]*0.05)
-        s="""update Score set '综测成绩'="""+str(k)+""" where 学号 = '"""+str(x[0])+"""'"""
+        s="""update ZScore set '综测成绩'="""+str(k)+""" where 学号 = '"""+str(x[0])+"""'"""
         print(s)
         cn.execute(s)
         cn.commit()
