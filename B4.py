d4cdec6dba026e9f0703df12bef1ea72b5c2a943
BLueberry_Pie <38281581+BLueberryPieBLue@users.noreply.github.com>
2019-01-11 22:49:26 +0800

    Update B4.py

--- B4.py
+++ B4.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 #
-#B4.py 综测成绩数据库操作
+#B4.py 导出"综测成绩.xls"
 #1.计算综测成绩
 #    总分=智育x0.7+思想品德得分（10%）+身心素质得分（5%）+创新实践能力得分（10%） +学院特色得分（5%）
 #2.排序
@@ -30,16 +30,16 @@ def order():
     s="""select * from ZScore order by "综测成绩" desc"""
     cursor=cn.execute(s)
     col_name_list = [tuple[0] for tuple in cursor.description]
-    #print (col_name_list)
+    print (col_name_list)
     for i in range(len(col_name_list)):
         sheet1.write(0,i,col_name_list[i],style)
-        #print(col_name_list[i])
+        print(col_name_list[i])
     a=1
     for row in cursor:
-        #print(row)
+        print(row)
         for i in range(len(row)):
             sheet1.write(a,i,row[i],style)
-            #print(row[i])
+            print(row[i])
         a=a+1   
     workbook.save('./综测成绩.xls')
     cur.close()
@@ -51,15 +51,15 @@ def B4():
     try:
         cn.execute('''alter table ZScore add COLUMN '综测成绩' DOUBLE''')
     except:
-        #print('Table has Exist')
-        pass
+        print('Table has Exist')
+
 
     while True:
         x=cur.fetchone()
         if not x:break
         k=(x[10]*0.7)+(x[3]*0.1)+(x[5]*0.05)+(x[7]*0.1)+(x[9]*0.05)
         s="""update ZScore set '综测成绩'="""+str(k)+""" where 学号 = '"""+str(x[0])+"""'"""
-        #print(s)
+        print(s)
         cn.execute(s)
         cn.commit()
     order()
